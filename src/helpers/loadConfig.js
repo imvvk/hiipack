5898792f63a2c645de42ed3627efd317be0bb4e9
kai.wei <kai.wei@qunar.com>
2017-02-17 09:04:01 +0800

    loadConfig

--- src/helpers/loadConfig.js
+++ src/helpers/loadConfig.js
@@ -7,5 +7,54 @@
 var fs = require("fs");
 var path = require("path");
 
-var projects_config = {};
+var PROJECTS_CONFIG = {};
 
+var log = global.log;
+
+module.exports = {
+    
+    init : function () {
+        var cwd = process.cwd;
+        var config = _load(cwd);
+        var projects;
+
+        if (config) {
+            projects = config.projects;
+
+            if (projects) {
+                PROJECTS_CONFIG.__isWorkspace = true;
+                var pc = PROJECTS_CONFIG.projects ={};
+                var prefixPathRegs = PROJECTS_CONFIG.regs = [];
+                Object.keys(projects_config).forEach(function (key) {
+                    pc[key] = _resolve(_load(path.join(cwd, projects[key])));
+                    prefixPathRegs.push(new RegExp("^/"+key+"/"));
+                });
+                console.log("regx=", prefixPathRegs);
+            } else {
+                PROJECTS_CONFIG.__isWorkspace = false;
+                PROJECTS_CONFIG.__default = _resolve(config);
+            }
+        }
+
+        function _load(p) {
+            var content, config;
+            try {
+                content = fs.readFileSync(path.join(p, "./hii.config.js"), "utf-8");
+                config = JSON.parse(content);
+            } catch(e) {
+                log.error("load config fail in ", p );
+                log.error("error : ", e);
+            }
+            return config;
+        }
+
+        function _resolve(config) {
+            if (!config) {
+                return null;
+            }
+             
+        }
+    }
+
+
+}
